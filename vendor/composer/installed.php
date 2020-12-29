05ea527d219101287965fc1ec69c3af5539b3219
Prakhar Tamta <prakhartamta@gmail.com>
2020-12-29 20:36:54 +0530

    update in composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '89146e440a915116ce1c9bf1521a18eec3ee3343',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '89146e440a915116ce1c9bf1521a18eec3ee3343',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+    ),
+  ),
+);
